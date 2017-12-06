300526144988b8b81dd2773bdb196db5b81e6966
Ghalib-Yousufi <ghalib.yousufi@venturedive.com>
2017-12-06 14:44:42 +0500

    Authentication done

---
+++ tutorial/quickstart/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+class Ticket(models.Model):
+	SEVERITY_CHOICES = (
+		(1, 'Major'),
+		(2, 'Critical'),
+		(3, 'Minor')
+	)
+	created = models.DateTimeField(auto_now_add=True)
+	description = models.CharField(max_length=100, blank=True, default='')
+	severity = models.CharField(max_length=20,choices=SEVERITY_CHOICES, default='')
+	assignee = models.ForeignKey('auth.User', related_name='assignee', blank=True)
+	reporter = models.ForeignKey('auth.User', related_name='reporter', blank=True)
+	is_done = models.BooleanField(default=False)
+	done_date = models.DateTimeField(null=True)
+
+	class Meta:
+		ordering = ('created',)
---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+from django.contrib.auth.models import User, Group
+from tutorial.quickstart.models import Ticket
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer, TicketSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
